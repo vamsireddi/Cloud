99a3b7eea128b032a84fc4f764b9f026283ee694
VamsidharReddy <ramavamsi49@gmail.com>
2023-12-05 08:41:38 +0530

    Sample commit1

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','bloodstore.mysql.database.azure.com');
+define('DB_USER','ram');
+define('DB_PASS','pass@1234567');
 define('DB_NAME','bbdms');
 // Establish database connection.
 try
